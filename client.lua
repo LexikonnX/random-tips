ec5a72d93a6cf11e6ca48a06f0bedc92a6a3319b
Lexikonn <108435506+LexikonnX@users.noreply.github.com>
2022-08-19 14:40:06 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,19 @@
+Citizen.CreateThread(function()
+    while true do
+        local interval = 30 -- in minutes
+        local tips = {
+            {value1 = "Join our ~g~Discord", value2 = ""},
+            {value1 = "Need admin?", value2 = "~y~/report"}
+        }
+        local rand = math.random(1,getTableSize(tips))
+        ESX.ShowAdvancedNotification('RANDOM TIP', tips[rand].value1,tips[rand].value2, "CHAR_DEFAULT", 8)
+        Citizen.Wait(interval*1000*60)
+    end
+end)
+function getTableSize(t)
+    local count = 0
+    for _, __ in pairs(t) do
+        count = count + 1
+    end
+    return count
+end
