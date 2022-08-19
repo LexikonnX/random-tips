e08683c982946cffcb11d24dce52533a2fa99a0e
Lexikonn <108435506+LexikonnX@users.noreply.github.com>
2022-08-19 14:40:40 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,10 @@
+local interval = 30 -- in minutes
+local tips = {
+    {value1 = "Join our ~g~Discord", value2 = ""},
+    {value1 = "Need admin?", value2 = "~y~/report"}
+}
 Citizen.CreateThread(function()
     while true do
-        local interval = 30 -- in minutes
-        local tips = {
-            {value1 = "Join our ~g~Discord", value2 = ""},
-            {value1 = "Need admin?", value2 = "~y~/report"}
-        }
         local rand = math.random(1,getTableSize(tips))
         ESX.ShowAdvancedNotification('RANDOM TIP', tips[rand].value1,tips[rand].value2, "CHAR_DEFAULT", 8)
         Citizen.Wait(interval*1000*60)
